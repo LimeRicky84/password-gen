eb465df784b210289feba67b47c91eccf8d25623
Eric Madsen <102508516+LimeRicky84@users.noreply.github.com>
2022-05-03 23:26:12 -0600

    Add files via upload

---
+++ 04-30 - Javascript - Challenge 3/Develop/script.js
@@ -0,0 +1,37 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+function lowerCase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+function upperCase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function numbers(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function symbols(){
+  const symbols = '!@#$%^&*'
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+console.log(lowerCase())
+console.log(upperCase())
+console.log(numbers())
+console.log(symbols())
