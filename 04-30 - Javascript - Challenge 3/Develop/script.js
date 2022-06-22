6af6dbdf0754067b214e3f43739eb0a1c9242e98
Ricma <ric.madse@gmail.com>
2022-06-22 01:36:30 -0600

    down to the last bug

--- 04-30 - Javascript - Challenge 3/Develop/script.js
+++ 04-30 - Javascript - Challenge 3/Develop/script.js
@@ -1,13 +1,7 @@
 var lengthEl = document.getElementById('pword')
-var lowercaseEl = document.getElementById("lowercase")
-var uppercaseEl = document.getElementById('uppercase')
-var numberEl = document.getElementById('number')
-var symbolEl = document.getElementById('symbol')
+
 var passDisplayEl = document.getElementById('password')
-var passLength = +lengthEl.value
-var hasUpper = uppercaseEl.checked
-var hasNumber = numberEl.checked
-var hasSymbol = symbolEl.checked
+let passLength = lengthEl.value
 
 const characterCodes = Array.from(Array(26)).map( (_, i) => i +97);
 
@@ -16,30 +10,51 @@ const randUpperCase = randLowerCase.map(letter => letter.toUpperCase())
 const randNumbers = [1,2,3,4,5,6,7,8,9,0]
 const randSymbols = ['!','@','#','$','%','&','*']
 
-// console.log(passLength, hasUpper, hasNumber, hasSymbol)  
-// console.log(randLowerCase, randUpperCase, randSymbols, randNumbers)
+  
 
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(passLength, hasUpper, hasNumber, hasSymbol){
+function generatePassword(){
 
-  var password = ""
+  const hasUpper = document.getElementById('uppercase').checked
+  const hasNumber = document.getElementById('number').checked
+  const hasSymbol = document.getElementById('symbol').checked
+  let passLength = lengthEl.value
+  console.log(passLength)
+  var finalPassword = ""
 
-  let availableChars = randLowerCase
-  if (hasUpper) availableChars = availableChars.concat(randUpperCase);
-  if (hasNumber) availableChars = availableChars.concat(randNumbers);
-  if (hasSymbol) availableChars = availableChars.concat(randSymbols);
-  
-  
+  let availableChars = []
+  availableChars = availableChars.concat(randLowerCase)
+  if (hasUpper){
+    availableChars = availableChars.concat(randUpperCase);
+  } 
+  if (hasNumber){
+    availableChars = availableChars.concat(randNumbers);
+  }
+  if (hasSymbol){
+    availableChars = availableChars.concat(randSymbols);
+  }
+
+  console.log(passLength, hasUpper, hasNumber, hasSymbol)
+  console.log(availableChars)
   for(let i=0; i < passLength; i++){
-  var mathIndex = availableChars[Math.floor(Math.random() * passLength)]
-  password = availableChars[mathIndex];
-  }; 
-  return password;
+    let finalPassword = availableChars[Math.floor(Math.random() * availableChars.length)]
+    
+    console.log(finalPassword)
+    passDisplayEl.innerHTML = finalPassword
+  }; return 
+  // console.log(finalPassword)
+  passDisplayEl.innerText = finalPassword
+  
 }
-console.log(generatePassword())
+// console.log(generatePassword())
 generateBtn.addEventListener('click', (Event) => {
+  
     Event.preventDefault()
-    var finalPassword = generatePassword(passLength.value)
-    passDisplayEl.innerText = finalPassword
+    console.log('clicked')
+    generatePassword()
+  
+  
   })
+
+
