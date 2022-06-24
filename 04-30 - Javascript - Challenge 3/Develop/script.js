9ef6bb8e8b7be0a9550bd2382e2eca3934f42f6d
Ricma <ric.madse@gmail.com>
2022-06-23 22:12:41 -0600

    works as intended

--- 04-30 - Javascript - Challenge 3/Develop/script.js
+++ 04-30 - Javascript - Challenge 3/Develop/script.js
@@ -1,29 +1,27 @@
-var lengthEl = document.getElementById('pword')
 
+// variables pulled from HTML
+var lengthEl = document.getElementById('pword')
 var passDisplayEl = document.getElementById('password')
 
-
 const characterCodes = Array.from(Array(26)).map( (_, i) => i +97);
-
 const randLowerCase = characterCodes.map(code => String.fromCharCode(code))
 const randUpperCase = randLowerCase.map(letter => letter.toUpperCase())
-const randNumbers = [1,2,3,4,5,6,7,8,9,0]
+const randNumbers = ['1','2','3','4','5','6','7','8','9','0']
 const randSymbols = ['!','@','#','$','%','&','*']
-
-  
 
 var generateBtn = document.querySelector("#generate");
 
+// this function checks whether attribute boxes are checked,
+// generates the desired password, and returns it to the page
 function generatePassword(){
 
   const hasUpper = document.getElementById('uppercase').checked
   const hasNumber = document.getElementById('number').checked
   const hasSymbol = document.getElementById('symbol').checked
   let passLength = lengthEl.value
-  console.log(passLength)
-  var generatedPassword = ""
-
+  let finalPassword = ""
   let availableChars = []
+
   availableChars = availableChars.concat(randLowerCase)
   if (hasUpper){
     availableChars = availableChars.concat(randUpperCase);
@@ -35,28 +33,21 @@ function generatePassword(){
     availableChars = availableChars.concat(randSymbols);
   }
 
-  console.log(passLength, hasUpper, hasNumber, hasSymbol)
-  console.log(availableChars)
+
   for(let i=0; i < passLength; i++){
-    let generatedPassword = availableChars[Math.floor(Math.random() * availableChars.length)]
-    
-    console.log(generatedPassword)
-    let finalPassword = generatedPassword.slice(0,passLength)
-    // console.log(finalPassword)
+    let generatedPassword = availableChars[Math.floor(Math.random() * availableChars.length)]  
+    finalPassword += generatedPassword.slice(0,passLength)
     passDisplayEl.innerText = finalPassword
   }; return  
-  // console.log(finalPassword)
-  passDisplayEl.innerText = finalPassword
-  
+
 }
-// console.log(generatePassword())
+// generate button functions
 generateBtn.addEventListener('click', (Event) => {
   
     Event.preventDefault()
     console.log('clicked')
     generatePassword()
-  
-  
+
   })
 
 
---
+++ README.md
@@ -0,0 +1,17 @@
+# Password Generator
+
+  ## GitHub
+
+    https://github.com/LimeRicky84
+
+  ## Description
+
+    Web app that generates passwords
+
+    GitHub link: https://github.com/LimeRicky84/password-gen
+
+    Live Page:  https://limericky84.github.io/password-gen/
+
+  ## Usage
+
+    Choose the character length, and whether to use uppercase, numbers and/or symbols and then click the generate button, once, or as many times as you feel like you need to.
